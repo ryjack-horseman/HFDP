8f1ec1224174df4e949cd8714bce56bbf3931a26
Ryan Jackson <ryanejackson97@yahoo.com>
2023-01-12 15:02:31 -0500

    Removed concrete menu references, now the waitress is just handed an ArrayList of Menus.

--- IteratorCompositePattern/src/Waitress.java
+++ IteratorCompositePattern/src/Waitress.java
@@ -1,28 +1,17 @@
+import java.util.ArrayList;
 import java.util.Iterator;
 public class Waitress {
-    Menu pancakeHouseMenu;
-    Menu dinerMenu;
-    Menu cafeMenu;
-
-    public Waitress(Menu pancakeHouseMenu, Menu dinerMenu, Menu cafeMenu) {
-        this.pancakeHouseMenu = pancakeHouseMenu;
-        this.dinerMenu = dinerMenu;
-        this.cafeMenu = cafeMenu;
+    ArrayList<Menu> menus;
+    public Waitress(ArrayList<Menu> menus) {
+       this.menus = menus;
     }
 
     public void printMenu(){
-        Iterator<MenuItem> pancakeIterator = pancakeHouseMenu.createIterator();
-        Iterator<MenuItem> dinerIterator = dinerMenu.createIterator();
-        Iterator<MenuItem> cafeIterator = cafeMenu.createIterator();
-
-        System.out.println("MENU\n----\nBREAKFAST");
-        printMenu(pancakeIterator);
-
-        System.out.println("\nLUNCH");
-        printMenu(dinerIterator);
-
-        System.out.println("\nDINNER");
-        printMenu(cafeIterator);
+       Iterator<Menu> menuIterator = menus.iterator();
+       while(menuIterator.hasNext()) {
+           Menu menu = menuIterator.next();
+           printMenu(menu.createIterator());
+       }
     }
 
     private void printMenu(Iterator iterator){
