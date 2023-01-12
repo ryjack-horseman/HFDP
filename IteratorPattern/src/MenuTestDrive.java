9893cd1c234460f383b9f5b63727b82c43c4bc75
Ryan Jackson <ryanejackson97@yahoo.com>
2023-01-12 15:20:22 -0500

    Updated the folder to just IteratorPattern, will do parallel Composite Pattern.

--- IteratorPattern/src/MenuTestDrive.java
+++ IteratorPattern/src/MenuTestDrive.java
@@ -1,10 +1,18 @@
+import java.util.ArrayList;
+
 public class MenuTestDrive {
     public static void main(String[] args) {
         PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
         DinerMenu dinerMenu = new DinerMenu();
         CafeMenu cafeMenu = new CafeMenu();
 
-        Waitress waitress = new Waitress(pancakeHouseMenu, dinerMenu, cafeMenu);
+        ArrayList<Menu> menu = new ArrayList<>();
+        menu.add(pancakeHouseMenu);
+        menu.add(dinerMenu);
+        menu.add(cafeMenu);
+
+
+        Waitress waitress = new Waitress(menu);
         waitress.printMenu();
     }
 }
