eed89f409653874582bc072c1b5d51a51d607de8
Ryan <rjackson@mobis-usa.com>
2023-04-13 10:06:13 -0400

    Added test harness and bug fix for connecting listeners

--- ObserverPattern/src/WeatherData.java
+++ ObserverPattern/src/WeatherData.java
@@ -37,5 +37,6 @@ public class WeatherData implements Subject {
         this.temperature = temperature;
         this.humidity = humidity;
         this.pressure = pressure;
+        measurementsChanged();
     }
 }
